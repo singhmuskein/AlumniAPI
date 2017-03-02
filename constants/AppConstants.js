9b2ef243d1a08383f42a73c87ac2ba6c0bf12ae9
Muskein Singh <muskein.singh@flipkart.com>
2017-03-03 01:20:10 +0530

    changed string

--- constants/AppConstants.js
+++ constants/AppConstants.js
@@ -2,6 +2,6 @@
  * Created by muskein.singh on 02/03/17.
  */
 var constant = {
-    MONGO_DB_URL: 'mongodb://muskeinsingh:Flipkart211293!@alumnidatabase-shard-00-00-wfd84.mongodb.net:27017,alumnidatabase-shard-00-01-wfd84.mongodb.net:27017,alumnidatabase-shard-00-02-wfd84.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=AlumniDatabase-shard-0&authSource=admin'
+    MONGO_DB_URL: 'mongodb://muskeinsingh:Flipkart211293!@alumnidatabase-shard-00-00-wfd84.mongodb.net:27017,alumnidatabase-shard-00-01-wfd84.mongodb.net:27017,alumnidatabase-shard-00-02-wfd84.mongodb.net:27017/alumnidatabase?ssl=true&replicaSet=AlumniDatabase-shard-0&authSource=admin'
 }
 module.exports = constant;
